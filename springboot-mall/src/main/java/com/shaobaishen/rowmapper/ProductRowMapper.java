331323a95e4254f0f06a5ff2b20195df34204a6d
shaobaishen <shaobai.shen@gmail.com>
2022-09-30 13:22:42 +0800

    商品功能 - 設定返回前端 時間格式、時區

--- springboot-mall/src/main/java/com/shaobaishen/rowmapper/ProductRowMapper.java
+++ springboot-mall/src/main/java/com/shaobaishen/rowmapper/ProductRowMapper.java
@@ -13,6 +13,7 @@ public class ProductRowMapper implements RowMapper {
     @Override
     public Product mapRow(ResultSet rs, int rowNum) throws SQLException {
         Product product = new Product();
+        product.setProduct_id(rs.getInt("product_id"));
         product.setPrice(rs.getInt("price"));
         product.setProduct_name(rs.getString("product_name"));
 
@@ -21,13 +22,12 @@ public class ProductRowMapper implements RowMapper {
         product.setCategory(productCategory);
 //        product.setCategory(ProductCategory.valueOf(rs.getString("category")));
 
-        product.setDescription(rs.getString("description"));
-        product.setCreated_date(rs.getDate("created_date"));
-        product.setLast_modified_date(rs.getDate("last_modified_date"));
-        product.setStock(rs.getInt("stock"));
         product.setImage_url(rs.getString("image_url"));
+        product.setStock(rs.getInt("stock"));
         product.setProduct_id(rs.getInt("product_id"));
-
+        product.setDescription(rs.getString("description"));
+        product.setCreated_date(rs.getTimestamp("created_date"));
+        product.setLast_modified_date(rs.getTimestamp("last_modified_date"));
         return product;
     }
 }
