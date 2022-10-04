efcf10bf1beee265dd0ed61a005b6080a29e24cc
shaobaishen <shaobai.shen@gmail.com>
2022-10-04 15:31:43 +0800

    帳號功能 - 檢查註冊的 email、隱藏返回的密碼

--- springboot-mall/src/main/java/com/shaobaishen/dao/impl/UserDaoImpl.java
+++ springboot-mall/src/main/java/com/shaobaishen/dao/impl/UserDaoImpl.java
@@ -24,6 +24,22 @@ public class UserDaoImpl implements UserDao {
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
+    public User getUserByEmail(String email) {
+        String sql = "SELECT user_id ,email,password,created_date,last_modified_date FROM user WHERE email=:email";
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("email", email);
+
+        List<User> userList = namedParameterJdbcTemplate.query(sql, map, new UserRowMapper());
+
+        if (userList.size() > 0) {
+            return userList.get(0);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
     public User getUserById(Integer userId) {
         String sql = "SELECT user_id ,email,password,created_date,last_modified_date FROM user WHERE user_id=:userId";
 
--- springboot-mall/src/main/java/com/shaobaishen/service/impl/UserServiceImpl.java
+++ springboot-mall/src/main/java/com/shaobaishen/service/impl/UserServiceImpl.java
@@ -4,11 +4,17 @@ import com.shaobaishen.dao.UserDao;
 import com.shaobaishen.dto.UserRegisterRequest;
 import com.shaobaishen.model.User;
 import com.shaobaishen.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao userDao;
@@ -17,8 +23,19 @@ public class UserServiceImpl implements UserService {
     public User getUserById(Integer userId) {
         return userDao.getUserById(userId);
     }
+
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+
+        //  檢查註冊的 email
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if (user != null) {
+            log.warn("這個 emil {} 已被註冊", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        //  創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 
